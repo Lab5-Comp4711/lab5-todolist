8a5e769a16b46cde778363749b45b725eeaf45cf
ktaaam <kevint4m@gmail.com>
2017-03-07 17:28:56 -0800

    Sync with team repo
    Signed-off-by: ktaaam <kevint4m@gmail.com>

--- application/controllers/Mtce.php
+++ application/controllers/Mtce.php
@@ -24,6 +24,11 @@ class Mtce extends Application {
 		    // and then pass them on
 		    $this->data['pagebody'] = 'itemlist';
 		    $this->render();
+
+		    if ($role == ROLE_OWNER)
+        		$result .= $this->parser->parse('oneitemx', (array) $task, true);
+			else
+        		$result .= $this->parser->parse('oneitem', (array) $task, true);
 		}
 
 			// Extract & handle a page of items, defaulting to the beginning
@@ -44,6 +49,11 @@ class Mtce extends Application {
 		        if ($count >= $this->items_per_page) break;
 		    }
 		    $this->data['pagination'] = $this->pagenav($num);
+		    //$this->show_page($tasks);
+
+		    $role = $this->session->userdata('userrole');
+		    if ($role == ROLE_OWNER) 
+		        $this->data['pagination'] .= $this->parser->parse('itemadd',[], true);
 		    $this->show_page($tasks);
 		}
 
